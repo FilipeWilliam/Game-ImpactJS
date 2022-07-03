471810452f04075078fbedf1fc5cec9d266a4a7d
Filipe William <lipe.will@outlook.com>
2022-07-03 00:08:50 -0300

    Adiciona a movimentação apenas do jogador atual

--- public/lib/game/entities/basePlayer.js
+++ public/lib/game/entities/basePlayer.js
@@ -69,73 +69,73 @@ ig.module(
 
         var accel = this.standing ? this.accelGround : this.accelAir;
 
-        if (ig.input.state('left')) {
-          this.moveLeft(accel);
-        } else if (ig.input.state('right')) {
-          this.moveRight(accel);
-        } else {
-          this.accel.x = 0;
-        }
-
-        if (this.standing && ig.input.state('jump') && this.vel.y == 0) {
-          this.vel.y = -this.jump;
-          this.falling = false;
-        } else if (!this.standing && !ig.input.state('jump') && !this.falling) {
-          this.vel.y = Math.floor(this.vel.y / 3);
-          this.falling = true;
-        }
-
-
-        if (this.isDying) {
-          if (!this.isInDiyngAnimation) {
-            this.currentAnim = this.anims.die.rewind();
-            this.isInDiyngAnimation = true;
-          } else if (this.currentAnim.loopCount > 0) {
-            this.kill();
-            this.isDying = false;
-            this.isInDiyngAnimation = false;
+        if(this.socketId === currentSocketId) {
+          if (ig.input.state('left')) {
+            this.moveLeft(accel);
+          } else if (ig.input.state('right')) {
+            this.moveRight(accel);
+          } else {
+            this.accel.x = 0;
           }
 
-        } else if (this.isTakeDamage) {
-          if (!this.isInTakeDamageAnimation) {
-            this.currentAnim = this.anims.receiveDamage.rewind();
-            this.isInTakeDamageAnimation = true;
-            this.receiveDamage(1);
-          } else if (this.currentAnim.loopCount > 0) {
-            this.isTakeDamage = false;
-            this.isInTakeDamageAnimation = false;
+          if (this.standing && ig.input.state('jump') && this.vel.y == 0) {
+            this.vel.y = -this.jump;
+            this.falling = false;
+          } else if (!this.standing && !ig.input.state('jump') && !this.falling) {
+            this.vel.y = Math.floor(this.vel.y / 3);
+            this.falling = true;
           }
-
-        } else if (!this.cooldownIsLoading && this.standing && ig.input.state('attack1')) {
-          this.isAttacking = true;
-          this.currentAnim = this.anims.attack1.rewind();
-          let fireBall;
-
-          if (!this.isInCooldownAttack) {
-            this.handleAttack();
-            this.isInCooldownAttack = true;
-
-          } else if (!this.cooldownIsLoading) {
-            this.cooldownIsLoading = true;
-            setTimeout(() => {
-              this.isInCooldownAttack = false;
-              this.cooldownIsLoading = false;
-            }, 1000);
+  
+          if (this.isDying) {
+            if (!this.isInDiyngAnimation) {
+              this.currentAnim = this.anims.die.rewind();
+              this.isInDiyngAnimation = true;
+            } else if (this.currentAnim.loopCount > 0) {
+              this.kill();
+              this.isDying = false;
+              this.isInDiyngAnimation = false;
+            }
+  
+          } else if (this.isTakeDamage) {
+            if (!this.isInTakeDamageAnimation) {
+              this.currentAnim = this.anims.receiveDamage.rewind();
+              this.isInTakeDamageAnimation = true;
+              this.receiveDamage(1);
+            } else if (this.currentAnim.loopCount > 0) {
+              this.isTakeDamage = false;
+              this.isInTakeDamageAnimation = false;
+            }
+  
+          } else if (!this.cooldownIsLoading && this.standing && ig.input.state('attack1')) {
+            this.isAttacking = true;
+            this.currentAnim = this.anims.attack1.rewind();
+            let fireBall;
+  
+            if (!this.isInCooldownAttack) {
+              this.handleAttack();
+              this.isInCooldownAttack = true;
+  
+            } else if (!this.cooldownIsLoading) {
+              this.cooldownIsLoading = true;
+              setTimeout(() => {
+                this.isInCooldownAttack = false;
+                this.cooldownIsLoading = false;
+              }, 1000);
+            }
+  
+  
+          } else if (!this.cooldownIsLoading && this.standing && !this.isAttacking && ig.input.state('attack2')) {
+            this.currentAnim = this.anims.attack2.rewind();
+          } else if (this.vel.y < 0 && !this.standing && !this.isAttacking) {
+            this.currentAnim = this.anims.jump;
+          } else if (this.vel.y > 0 && !this.standing && !this.isAttacking) {
+            this.currentAnim = this.anims.fall;
+          } else if (this.vel.x != 0 && !this.isAttacking) {
+            this.currentAnim = this.anims.run;
+          } else if (!this.isAttacking) {
+            this.currentAnim = this.anims.idle;
           }
-
-
-        } else if (!this.cooldownIsLoading && this.standing && !this.isAttacking && ig.input.state('attack2')) {
-          this.currentAnim = this.anims.attack2.rewind();
-        } else if (this.vel.y < 0 && !this.standing && !this.isAttacking) {
-          this.currentAnim = this.anims.jump;
-        } else if (this.vel.y > 0 && !this.standing && !this.isAttacking) {
-          this.currentAnim = this.anims.fall;
-        } else if (this.vel.x != 0 && !this.isAttacking) {
-          this.currentAnim = this.anims.run;
-        } else if (!this.isAttacking) {
-          this.currentAnim = this.anims.idle;
         }
-
 
         this.parent();
       },
